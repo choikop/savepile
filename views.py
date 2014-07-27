762c211c062d061bbb7d990cb23061d588159a08
choikop <chlwlghks200@naver.com>
2014-07-28 03:37:32 +0900

    no message

--- views.py
+++ views.py
@@ -1,16 +1,54 @@
-from flask import render_template, Flask, request
+from flask import render_template, Flask, request, url_for
 from apps import app
 
+from google.appengine.ext import db
+
+class Photo(db.Model):
+	photo = db.BlobProperty()
+	string = db.StringProperty(default='', multiline=True)
+
+
+
+def allowed_file(filename):
+	ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+
+	return '.' in filename and \
+	filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+
 @app.route('/')
-@app.route('/index', methods=['GET', 'POST'])
+@app.route('/index')
 def index():
-	get = None
-	post = None
+	return render_template("upload.html", all_list=Photo.all())
+
+
+
+@app.route('/upload', methods=['POST'])
+def upload_db():
+	post_data = request.files['photo']
+	text_data = request.forms['string']
+
+	if post_data and allowed_file(post_data.filename) and text_data:
+		filestream = post_data.read()
+
+		upload_data = Photo()
+		upload_data.photo = db.Blob(filestream)
+		upload_data.put()	
+
+		comment = "uploaded!"
+
 	
-	if request.args:
-		get = request.args['text_get']
+	else:
+		comment = "please upload valid image file"
 
-	if request.form:
-		post = request.form['text_post']
 
-	return render_template("index.html", variable_get = get, variable_post = post)
+	return render_template("upload.html", text_data=text_data, comment=comment, all_list=Photo.all())
+
+
+
+@app.route('/show/<key>')
+def shows(key):
+	uploaded_data = db.get(key)
+	return app.response_class(uploaded_data.photo)
+
+
